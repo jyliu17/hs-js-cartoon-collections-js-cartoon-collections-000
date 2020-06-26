3de9a796e948e0d75d8523b37971ffd0ed7bf15d
Johnson Liu <jyliu17@gmail.com>
2020-06-26 13:50:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
+
 function dwarfRollCall(dwarves) {
+  var array = [];
+  for (var i = 0; i < dwarves.length; i++) {
+    array.push(`${i+1}. ${dwarves[i]} `);
+  }
+  return array.join("");
 }
 
 function summonCaptainPlanet(planeteerCalls){
+ let capsPlaneteerCalls = [];
+  for(let i = 0; i < planeteerCalls.length; i++){
+    let exclamation = "!"
+    capsPlaneteerCalls.push(`${planeteerCalls[i]}${exclamation}`.toUpperCase())
+  }
+  return capsPlaneteerCalls
 }
 
+
 function longPlaneteerCalls(words) {
+
+  for (var i = 0; i < words.length; i++){
+    if (words[i].length > 4)
+    return true
+  } 
+      return false 
 }
 
 function findTheCheese (foods) {
+  
+  for (var i = 0; i < foods.length; i++){
+    if (foods[i] === "cheddar"  || foods[i] === "gouda" || foods[i] === "camembert")
+    return foods[i];
+  }
+   return "no cheese!"
 }
